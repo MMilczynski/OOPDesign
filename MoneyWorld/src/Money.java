75ef7abe842bc856501daf3afa01f8204a1eff6f
MMilczynski <matthias.milczynsii@gmail.com>
2022-01-13 06:44:04 -0800

    Example of implications of using getters and setters on adding functionality. Substantial changes to the code are required to accomodate change.

--- MoneyWorld/src/Money.java
+++ MoneyWorld/src/Money.java
@@ -1,13 +1,31 @@
+enum Currency {
+    USD, EURO;
+
+    /* ... just make it work, no logic here */
+    public double convertRateTo(Currency target) {
+        return 1.0;
+    }
+}
 
 public class Money {
     private double value;
 
-    public Money(double value) {
+    // need to add this
+    private Currency currency;
+
+    // need to modify constructor, could have used overloading as well
+    public Money(double value, Currency currency) {
         this.value = value;
+        // need to add this line
+        this.currency = currency;
     }
 
     public double getValue() {
         return value;
+    }
+
+    public Currency getCurrency() {
+        return this.currency;
     }
 
     public void setValue(double value) {
@@ -27,10 +45,23 @@ class Test {
     }
 
     public static void test() {
-        Money balance = new Money(6.0);
-        Money request = new Money(3.0);
+        /*
+         * previous code
+         * 
+         * Money balance = new Money(1.0);
+         * Money request = new Money(3.0);
+         * 
+         * now I need to modify those two calls and add conversion operation.
+         * Error-prone code and hard to maintain
+         */
+        Money balance = new Money(1.0, Currency.EURO);
+        Money request = new Money(3.0, Currency.USD);
 
-        if (balance.getValue() > request.getValue())
+        double normalizedBalance = balance.getValue() * balance.getCurrency().convertRateTo(Currency.USD);
+
+        double normalizedRequest = request.getValue() * request.getCurrency().convertRateTo(Currency.USD);
+
+        if (normalizedBalance > normalizedRequest)
             dispenseFunds(request);
     }
 }
