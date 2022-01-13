892d9674aef37fd81622c9e02aeced735cc75be7
MMilczynski <matthias.milczynsii@gmail.com>
2022-01-13 06:34:52 -0800

    Initial commit of simple java exaxmple demonstrating why getters ad setters are evil.

---
+++ MoneyWorld/src/Money.java
@@ -0,0 +1,85 @@
+//package MoneyWorld;
+
+// added currency enum
+enum Currency {
+    USD, EURO;
+
+    /* ... just make it work, no logic here */
+    public double convertRateTo(Currency target) {
+        return 1.0;
+    }
+}
+
+public class Money {
+    private double value;
+
+    // need to add this
+    private Currency currency;
+
+    // need to modify constructor, could have used overloading as well
+    public Money(double value, Currency currency) {
+        this.value = value;
+        // need to add this line
+        this.currency = currency;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public Currency getCurrency() {
+        return this.currency;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    // add those to methods to fix problem
+    private double normalize() {
+        return currency == Currency.USD
+                ? value
+                : value * currency.convertRateTo(Currency.USD);
+    }
+
+    public boolean isGreaterThan(Money op) {
+        return (normalize() > op.normalize());
+    }
+
+    public static void main(String[] args) {
+        Test.test();
+    }
+}
+
+class Test {
+
+    private static void dispenseFunds(Money amount) {
+        /* ... */
+        System.out.println("Funds dispensed");
+    }
+
+    public static void test() {
+        /*
+         * previous code
+         * 
+         * Money balance = new Money(1.0);
+         * Money request = new Money(3.0);
+         * 
+         * now I need to modify those two calls and add conversion operation.
+         * Error-prone code and hard to maintain
+         */
+        Money balance = new Money(6.0, Currency.EURO);
+        Money request = new Money(3.0, Currency.USD);
+
+        double normalizedBalance = balance.getValue() * balance.getCurrency().convertRateTo(Currency.USD);
+
+        double normalizedRequest = request.getValue() * request.getCurrency().convertRateTo(Currency.USD);
+
+        if (normalizedBalance > normalizedRequest)
+            dispenseFunds(request);
+
+        if (balance.isGreaterThan(request)) {
+            dispenseFunds(request);
+        }
+    }
+}
